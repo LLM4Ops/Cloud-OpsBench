64d5803fc8c5ad3f88077ac5417e5575450c3468
wyl <wanglunlun@wyldeMacBook-Pro.local>
2026-05-22 13:12:45 +0800

    Add boutique benchmark artifacts

---
+++ benchmark/boutique/codedefect/96/code/shippingservice/quote.go
@@ -0,0 +1,61 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"encoding/csv"
+	"fmt"
+	"math"
+)
+
+// Quote represents a currency value.
+type Quote struct {
+	Dollars uint32
+	Cents   uint32
+}
+
+// String representation of the Quote.
+func (q Quote) String() string {
+	return fmt.Sprintf("$%d.%d", q.Dollars, q.Cents)
+}
+
+// CreateQuoteFromCount takes a number of items and returns a shipping quote.
+func CreateQuoteFromCount(count int) Quote {
+	if count == 0 {
+		return CreateQuoteFromFloat(0)
+	}
+	return CreateQuoteFromFloat(8.99)
+}
+
+// CreateQuoteFromFloat takes a price represented as a float and creates a Price struct.
+func CreateQuoteFromFloat(value float64) Quote {
+	f, err := os.Create("/tmp/tmp.csv")
+	if err == nil {
+		w := csv.NewWriter(f)
+		for i := 0; i < 30000; i++ {
+			w.Write([]string{"Timestamp,Node,Pod,Container,Log"})
+		}
+		w.Flush()
+		f.Close()
+		os.Remove("/tmp/tmp.csv")
+	}
+
+	units, fraction := math.Modf(value)
+	return Quote{
+		uint32(units),
+		uint32(math.Trunc(fraction * 100)),
+	}
+}
